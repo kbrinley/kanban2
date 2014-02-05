7c3af85ecd17117425e7561cf3b0fe81c5bc5963
kbrinley <kbrinley@gmail.com>
2014-02-04 23:03:53 -0500

    Added SQLite integration and now creates a table, inserts a record, and queries the database.

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var kanban = require('./routes/kanban');
 var http = require('http');
 var path = require('path');
 
+var sqlite = require('sqlite3');
+var db = new sqlite.Database('kanban', createTables);
+
 var app = express();
 
 // all environments
@@ -36,3 +39,28 @@ app.get('/kanban', kanban.board);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Database functions
+function createTables() {
+ console.log('Create Tables in SQLite3 database if needed');
+    db.run("CREATE TABLE IF NOT EXISTS versions (version TEXT)", insertRows);
+}
+
+function insertRows() {
+ // Need to only insert if row doesn't currently exist.
+
+    console.log("Insert Rows for Sprint 0");
+    var stmt = db.prepare("INSERT INTO versions VALUES (?)");
+
+    stmt.run("Sprint 0");
+
+    stmt.finalize(queryDB);
+}
+
+function queryDB() {
+ db.all("SELECT rowid as id, version FROM versions", function(err, rows) {
+    rows.forEach(function(row) {
+        console.log(row.id + ": " + row.version);
+    });
+ });
+}
